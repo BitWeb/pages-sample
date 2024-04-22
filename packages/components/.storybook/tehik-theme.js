f84e23d545a4fa30a5d17ad0f7db7b8e8ed34241
Airike Jaska <airike.jaska@bitweb.ee>
2024-04-22 13:23:00 +0300

    fix(storybook): Fixed logo url path

--- packages/components/.storybook/tehik-theme.js
+++ packages/components/.storybook/tehik-theme.js
@@ -1,10 +1,16 @@
 import { create } from '@storybook/theming';
 
+function getImageUrl() {
+  const baseUrl = 'https://bitweb.github.io/pages-sample/';
+  const branch = process.env.CURRENT_BRANCH;
+  return `${baseUrl}${branch}/TEHIK_logo.svg`;
+}
+
 export default create({
   appBg: '#2a2a3c',
   base: 'dark',
   brandTitle: 'Tehik react components',
   brandUrl: 'https://disainipesa-react.tehik.ee/',
-  brandImage: './TEHIK_logo.svg',
+  brandImage: getImageUrl(),
   brandTarget: '_self',
 });
